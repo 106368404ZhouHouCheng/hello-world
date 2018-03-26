bd0c336d49c74a0f133ad35a9c8188b7a672bc5e
106368404ZhouHouCheng <32475450+106368404ZhouHouCheng@users.noreply.github.com>
2018-03-26 20:40:54 +0800

    ex09

---
+++ ex09.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+#Here's some new strange stuff, remember type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ",days
+print "Here are the months: ",months
+
+'''
+python 三引号允许一个字符串跨多行，字符串中可以包含换行符、制表符以及其他特殊字符
+'''
+print """
+There's something going on here.\
+\000 With the three double-quotes.\n
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or6.
+"""
