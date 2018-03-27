7b966940f0848cc34757b90f53af47b9919088e6
106368404ZhouHouCheng <32475450+106368404ZhouHouCheng@users.noreply.github.com>
2018-03-27 23:00:29 +0800

    Add files via upload

---
+++ ex07.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#print the string
+print "Mary had a little lamb."
+#print the string which followed the other string
+print "Its fleece was white as %s." % 'snow'
+# print
+print "And everywhere that Mary went."
+print "." * 20
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#watch that comma at the end. try removing it to see that what happens
+print end1+end2+end3+end4+end5+end6,
+print end7+end8+end9+end10+end11+end12
+
+a = "train"
+b = "test"
+
+print a,
+print b
